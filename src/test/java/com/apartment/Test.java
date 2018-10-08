a95cd980f2e3c19f7b9e801aab8c926fb7479f1a
srikanth <srikanth.j825@gmail.com>
2018-10-08 08:18:28 +0530

    test added

--- src/test/java/com/apartment/Test.java
+++ src/test/java/com/apartment/Test.java
@@ -1,12 +1,17 @@
 package com.apartment;
 
+import com.apartment.entity.Unit;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.URL;
 import java.util.List;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
 public class Test {
 
@@ -19,11 +24,53 @@ public class Test {
 
         // parses all rows in one go.
         Test test = new Test();
-        List<String[]> allRows = parser.parseAll(test.getReader("Units.csv"));
+        List<String[]> allRows = parser.parseAll(test.getReader("/Units"));
         System.out.print(allRows);
+        for (String[] s : allRows) {
+            Unit unit = new Unit();
+            unit.setBlock(s[0]);
+            unit.setFloor(s[1]);
+            unit.setFlatNo(s[2]);
+            unit.setSqft(Integer.valueOf(s[3]));
+            unit.setParkingSlots(Integer.valueOf(s[4]));
+            unit.setParkingSqft(Integer.valueOf(s[5]));
+            unit.setBillerName(s[6]);
+            unit.setUnitCategory(s[7]);
+            try {
+
+                Client client = Client.create();
+
+                WebResource webResource = client
+                        .resource("http://localhost:8080/unit/");
+                ObjectMapper mapper= new ObjectMapper();
+
+                String input = mapper.writeValueAsString(unit);
+                ClientResponse response = webResource.type("application/json")
+                        .post(ClientResponse.class, input);
+
+                if (response.getStatus() != 201) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + response.getStatus());
+                }
+
+                System.out.println("Output from Server .... \n");
+                String output = response.getEntity(String.class);
+                System.out.println(output);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+        }
+
+
     }
-    public static Reader getReader(String relativePath) {
+
+    public static Reader getReader(String relativePath) throws UnsupportedEncodingException {
         try {
+            //URL url = Test.class.getResource("application.properties");
+            //InputStream strm = url.openStream();
             return new InputStreamReader(Test.class.getResourceAsStream(relativePath), "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException("Unable to read input", e);
